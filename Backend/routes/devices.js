5ff9fbe7dd41333152ae4f0076d61f03b9ce46f0
ArnauRamon <arnauramon4@gmail.com>
2018-03-29 13:02:59 +0200

    Add files via upload

--- Backend/routes/devices.js
+++ Backend/routes/devices.js
@@ -1,5 +1,6 @@
 var mongoose = require ('mongoose');
 var device = mongoose.model('Device');
+
 // GET - Retorna tots els registres
 exports.findAll = function ( req,res) {
 	device.find(function(err,devices){
@@ -7,31 +8,59 @@ exports.findAll = function ( req,res) {
 	res.status(200).jsonp(devices);
 	});
 };
-//Get- Retorna un sol Registre
+
+//GET- Retorna un sol Registre per l'id del dispositiu
 exports.findDevice=function(req,res){
-	device.find({'name':req.params.name},function(err,device){
+	device.find({'id':req.params.id},function(err,Device){
+	
 	if(err) return res.send(500,err.message);
-	res.status(200).jsonp(device);
+	if(Device[0]!=null){
+		res.status(200).jsonp(Device);
+	}else{
+		res.status(400).jsonp("No Trobat");
+	}
 	});
 };
 
-//POST insrtar un nou registre 
+//POST - Afegir un nou registre 
 exports.add=function(req,res){
 	var Device= new device({
 		id:req.body.id,		
 		name:req.body.name,
 		latitude:req.body.latitude,
 		longitude:req.body.longitude,
-		creationDate:req.body.creationDate,
-		modificationDate:req.body.modificationDate,
+		creationDate:new Date(), //o posem nosaltres
+		modificationDate:new Date(), // o posem nosaltres ISO 8601
 	});
-	Device.save(function(err,Device){
-		if(err) return res.send(500,err.message);
-		res.status(200).json(Device);
+	//Comprovem que els valors siguien "raonables", si ho son fem el save.
+	if (Device.id && Device.name && Device.latitude && Device.longitude){
+		Device.save(function(err,Device){
+			if(err) return res.send(500,err.message);
+			res.status(200).json(Device);
+		});
+	}else{
+		res.status(400).jsonp("Camps NO introduïts correctament");
+	}
+	
+};
+
+//Delete - Elimina un registre per l'id del dispositiu
+exports.deleteDevice=function(req, res){
+	device.find({'id':req.params.id},function(err, Device){
+		if(err) return res.send(500, err.message);
+		if(Device[0]!=null){
+			Device[0].remove(function(err){
+				if(err) return res.send(500,err.message);
+				res.status(200).jsonp("Borrat correctament");
+			});
+		}else{
+			res.status(400).jsonp("No Trobat");
+		}
 	});
 };
-		
-//Delete borra tots els registres
+			
+
+//Delete - Elimina TOTS els registres
 exports.borra=function(req,res){
 	device.find(function(err,devices){
 		if(err) res.send(500,err.message);
