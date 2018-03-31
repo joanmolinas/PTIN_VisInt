6532758977c4387a42a786b8afbec93147649ca2
ArnauRamon <arnauramon4@gmail.com>
2018-03-31 12:50:17 +0200

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -2,11 +2,13 @@
  * This router file contains api routes.
  */
 
+var device = mongoose.model('Device');
+
 var express = require('express');
 var router = express.Router();
 
 /**
- *  Just and example of find query.
+ *  GET - Nombre de un usuario por "nombre"
  */
 router.get('/user/:nombre', function(req, res, next) {
   let user = require('../models/User')
@@ -19,7 +21,7 @@ router.get('/user/:nombre', function(req, res, next) {
 
 /**
  * @author ncarmona
- * @description Displays a add device form.
+ * @description Displays a add device form. Crida a un formulari semblant al de la web Postman
  * @version S1
  */
 router.get('/device/addform', function(req, res, next){
@@ -133,78 +135,5 @@ router.get('/device/getByID/:id', function(req, res, next){
   })
 
 })
-
-// GET - Retorna tots els registres
-exports.findAll = function ( req,res) {
-	device.find(function(err,devices){
-	if(err) res.send(500,err.message);
-	res.status(200).jsonp(devices);
-	});
-};
-
-//GET- Retorna un sol Registre per l'id del dispositiu
-exports.findDevice=function(req,res){
-	device.find({'id':req.params.id},function(err,Device){
-	
-	if(err) return res.send(500,err.message);
-	if(Device[0]!=null){
-		res.status(200).jsonp(Device);
-	}else{
-		res.status(400).jsonp("No Trobat");
-	}
-	});
-};
-
-//POST - Afegir un nou registre 
-exports.add=function(req,res){
-	var Device= new device({
-		id:req.body.id,		
-		name:req.body.name,
-		latitude:req.body.latitude,
-		longitude:req.body.longitude,
-		creationDate:new Date(), //o posem nosaltres
-		modificationDate:new Date(), // o posem nosaltres ISO 8601
-	});
-	//Comprovem que els valors siguien "raonables", si ho son fem el save.
-	if (Device.id && Device.name && Device.latitude && Device.longitude){
-		Device.save(function(err,Device){
-			if(err) return res.send(500,err.message);
-			res.status(200).json(Device);
-		});
-	}else{
-		res.status(400).jsonp("Camps NO introduïts correctament");
-	}
-	
-};
-
-//Delete - Elimina un registre per l'id del dispositiu
-exports.deleteDevice=function(req, res){
-	device.find({'id':req.params.id},function(err, Device){
-		if(err) return res.send(500, err.message);
-		if(Device[0]!=null){
-			Device[0].remove(function(err){
-				if(err) return res.send(500,err.message);
-				res.status(200).jsonp("Borrat correctament");
-			});
-		}else{
-			res.status(400).jsonp("No Trobat");
-		}
-	});
-};
-			
-
-//Delete - Elimina TOTS els registres
-exports.borra=function(req,res){
-	device.find(function(err,devices){
-		if(err) res.send(500,err.message);
-		while(devices[0]!=null){
-			devices[0].remove(function(err){
-				if(err) return res.send(500,err.message);
-				});
-				devices.shift();
-		};
-		res.json({message:"Elements borrats correctament"});
-	});
-};
 
 module.exports = router;
