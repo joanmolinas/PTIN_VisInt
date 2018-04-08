72831cd126e581b0cffd06cc9cfca920ada39e6d
Noel <ncarm89@gmail.com>
2018-04-08 10:19:51 +0200

    vue support and filter by text.

--- public/javascripts/visint.js
+++ public/javascripts/visint.js
@@ -1,12 +1,120 @@
-$( document ).ready(function() {
+window.addEventListener('load', function(){
+    new Vue({
+        el: '#sidebar',
+        data: {
+            base_url_api: 'http://localhost:3000/api/',
+            devices: [],
+            devices_column1: [],
+            devices_column2: [],
+            selected_device: '',
+            filter_text: '',
+            device_type: '*',
+            queryDelay: 1000            
+        },
+        mounted: function(){
+            return this.getDevices()
+        },
+        methods: {
+            /**
+             * @author ncarmona
+             * @description Get all devices and print them in right and left column at the sidebar.
+             * @version S2 - Removed this feature from routes/public.
+             * @todo add spinner while 
+             */
+            getDevices: function(){
+                let self = this
+                
+                axios.get(this.base_url_api + 'devices').then(function(response){
+                    self.devices = response.data
+                    self.devices.forEach(function(device, index){
+                        if(index % 2 == 0)
+                            self.devices_column1.push(device)
+                        else
+                            self.devices_column2.push(device)
+                        
+                    });
+                }).catch( function(error){
+                    console.log(error)
+                })
+            },
 
-    /*Hide sidebar.*/
-    $('a[role="hide-devices"]').click(function(){
-        $("#sidebar").hide()
-        $("#sidebar").removeClass('col-md-6')
-        $("#content").removeClass('col-md-6')
-        $("#content").removeClass('col-md-12')    
-        $("#content").css('height', '100vh')    
+            /**
+             * @author ncarmona
+             * @description Clear devices list.
+             * @version S2
+             * @todo add spinner while.
+             */            
+            removeDevicesFromList: function(){
+                devices = document.getElementById("devices-list")
+        
+                devices.getElementsByClassName("col-md-6")[0].innerHTML=''
+                devices.getElementsByClassName("col-md-6")[1].innerHTML=''                
+            },
+
+            /**
+             * @author ncarmona
+             * @description Filter devices by name.
+             * @version S2 - Removed this feature from routes/public.
+             * @todo add spinner while.
+             * @requires axios
+             */            
+            filterByText: function(){
+                if (this.filter_text.length == 0)
+                    this.getDevices()
+                else if (this.filter_text.length > 0 && this.filter_text.length < 3)
+                    console.log ("Nothing TO DO")
+                else{
+                    let self = this
+                    setTimeout(function(){
+                        // https://ptin2018.herokuapp.com/api/devices/?name=pepito
+                        axios.get(self.base_url_api + 'devices/?name=' + self.filter_text).then(function(response){
+                            self.removeDevicesFromList()
+                            self.devices = response.data
+                            self.devices.forEach(function(device, index){
+                                
+                                if(index % 2 == 0)
+                                    self.devices_column1.push(device)
+                                else
+                                    self.devices_column2.push(device)
+                            });
+                        }).catch( function(error){
+                            console.log(error.message)
+                        })
+                    }, this.queryDelay)
+                }
+
+            },
+
+            /**
+             * @author 
+             * @description
+             * @version 
+             */            
+            filterByType: function(){
+                this.removeDevicesFromList()
+                console.log ("Llamamos AJAX")                
+            },
+
+            /**
+             * @author ncarmona
+             * @description Hide sidebar and expand the map layer.
+             * @version S2 - Integrated in vuejs with pure js.
+             */            
+            expandMap: function(){
+                document.getElementById("sidebar").style.display = "none"
+                document.getElementById("content").className = "col-md-12"  
+                console.log(this.selected_device)              
+            },
+
+            /**
+             * @author ncarmona
+             * @description Display the sidebar if the map has been expanded.
+             * @version S2
+             */            
+            shrinkMap: function(){
+                document.getElementById("sidebar").style.display = "block"
+                document.getElementById("content").className = "col-md-6"                
+            }
+        }
     })
-
-});
+})
--- routes/public.js
+++ routes/public.js
@@ -6,33 +6,14 @@ var express = require('express');
 var router = express.Router();
 var fs = require('fs')
 var path = require('path');
-var request = require('request')
-
-var default_lang = JSON.parse(fs.readFileSync(path.join(__dirname, '..', 'config.json'))).language.default_language
 
 /**
  * @author ncarmona
  * @description Display the map and devices.
  * @version S1
- * @requires request
  */
 router.get('/', function(req, res, next) {
-
-    var trans = JSON.parse(fs.readFileSync(path.join(__dirname, '..', 'lang', default_lang, 'public.json')))
-
-    // Getting all devices using api.
-    request({
-        url: "https://ptin2018.herokuapp.com/api/devices",
-        json: true
-    }, function (error, response, body) {
-        if (!error && response.statusCode === 200) {
-            console.log(body)
-            res.render('index-public', {
-                'trans': trans.map_display,
-                'content': body
-              })
-        }
-    })
+    res.sendFile()
 });
 
 module.exports = router;
