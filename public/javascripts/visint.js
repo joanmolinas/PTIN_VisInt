4a7f777b3ea9fc1b9097099940a18b27b31c7e22
ricardTorrents <torrents279@gmail.com>
2018-04-10 12:23:56 +0200

    Map added with opwnLayer

--- public/javascripts/visint.js
+++ public/javascripts/visint.js
@@ -1,4 +1,4 @@
-window.addEventListener('load', function(){
+window.addEventListener('load', function () {
     new Vue({
         el: '#sidebar',
         data: {
@@ -9,9 +9,16 @@ window.addEventListener('load', function(){
             selected_device: '',
             filter_text: '',
             device_type: '*',
-            queryDelay: 1000            
+            queryDelay: 1000,
+            zoomInicial: 18,
+            maxzm: 20,
+            minzm: 17,
+            vectorLayer: '',
+            iconstyle: '',
+            map: ''
         },
-        mounted: function(){
+        mounted: function () {
+            this.loadMap()
             return this.getDevices()
         },
         methods: {
@@ -21,19 +28,19 @@ window.addEventListener('load', function(){
              * @version S2 - Removed this feature from routes/public.
              * @todo add spinner while 
              */
-            getDevices: function(){
+            getDevices: function () {
                 let self = this
-                
-                axios.get(this.base_url_api + 'devices').then(function(response){
+
+                axios.get(this.base_url_api + 'devices').then(function (response) {
                     self.devices = response.data
-                    self.devices.forEach(function(device, index){
-                        if(index % 2 == 0)
+                    self.devices.forEach(function (device, index) {
+                        if (index % 2 == 0)
                             self.devices_column1.push(device)
                         else
                             self.devices_column2.push(device)
-                        
+
                     });
-                }).catch( function(error){
+                }).catch(function (error) {
                     console.log(error)
                 })
             },
@@ -43,12 +50,12 @@ window.addEventListener('load', function(){
              * @description Clear devices list.
              * @version S2
              * @todo add spinner while.
-             */            
-            removeDevicesFromList: function(){
+             */
+            removeDevicesFromList: function () {
                 devices = document.getElementById("devices-list")
-        
-                devices.getElementsByClassName("col-md-6")[0].innerHTML=''
-                devices.getElementsByClassName("col-md-6")[1].innerHTML=''                
+
+                devices.getElementsByClassName("col-md-6")[0].innerHTML = ''
+                devices.getElementsByClassName("col-md-6")[1].innerHTML = ''
             },
 
             /**
@@ -57,27 +64,27 @@ window.addEventListener('load', function(){
              * @version S2 - Removed this feature from routes/public.
              * @todo add spinner while.
              * @requires axios
-             */            
-            filterByText: function(){
+             */
+            filterByText: function () {
                 if (this.filter_text.length == 0)
                     this.getDevices()
                 else if (this.filter_text.length > 0 && this.filter_text.length < 3)
-                    console.log ("Nothing TO DO")
-                else{
+                    console.log("Nothing TO DO")
+                else {
                     let self = this
-                    setTimeout(function(){
+                    setTimeout(function () {
                         // https://ptin2018.herokuapp.com/api/devices/?name=pepito
-                        axios.get(self.base_url_api + 'devices/?name=' + self.filter_text).then(function(response){
+                        axios.get(self.base_url_api + 'devices/?name=' + self.filter_text).then(function (response) {
                             self.removeDevicesFromList()
                             self.devices = response.data
-                            self.devices.forEach(function(device, index){
-                                
-                                if(index % 2 == 0)
+                            self.devices.forEach(function (device, index) {
+
+                                if (index % 2 == 0)
                                     self.devices_column1.push(device)
                                 else
                                     self.devices_column2.push(device)
                             });
-                        }).catch( function(error){
+                        }).catch(function (error) {
                             console.log(error.message)
                         })
                     }, this.queryDelay)
@@ -89,31 +96,93 @@ window.addEventListener('load', function(){
              * @author 
              * @description
              * @version 
-             */            
-            filterByType: function(){
+             */
+            filterByType: function () {
                 this.removeDevicesFromList()
-                console.log ("Llamamos AJAX")                
+                console.log("Llamamos AJAX")
             },
 
             /**
              * @author ncarmona
              * @description Hide sidebar and expand the map layer.
              * @version S2 - Integrated in vuejs with pure js.
-             */            
-            expandMap: function(){
+             */
+            expandMap: function () {
                 document.getElementById("sidebar").style.display = "none"
-                document.getElementById("content").className = "col-md-12"  
-                console.log(this.selected_device)              
+                document.getElementById("content").className = "col-md-12"
+                console.log(this.selected_device)
             },
 
             /**
              * @author ncarmona
              * @description Display the sidebar if the map has been expanded.
              * @version S2
-             */            
-            shrinkMap: function(){
+             */
+            shrinkMap: function () {
                 document.getElementById("sidebar").style.display = "block"
-                document.getElementById("content").className = "col-md-6"                
+                document.getElementById("content").className = "col-md-6"
+            },
+            loadMap: function () {
+                //Crea capa vectorial, amb dos Features que son dos punts geometrics
+                this.vectorLayer = new ol.layer.Vector({
+                    name: "vector",
+                    source: new ol.source.Vector({
+                        features: [
+                            new ol.Feature({
+                                name: 'p1',
+                                geometry: new ol.geom.Point([1.7300289, 41.2219107])
+                            }),
+                            new ol.Feature({
+                                name: 'p2',
+                                geometry: new ol.geom.Point([1.7300289, 41.2209107])
+                            })
+                        ]
+                    })
+
+                });
+
+
+                //Defineix l'estil del punt que col·loquem al mapa.
+                /*this.iconStyle = new ol.style.Style({
+                        image: new ol.style.Icon( /** @type {olx.style.IconOptions} */// ({
+			    /*	anchor: [0.5, 46],
+			    	anchorXUnits: 'fraction',
+			    	anchorYUnits: 'pixels',
+			    	opacity: 0.75,
+			    	src: 'icon.png'
+	    		}))
+    		});*/
+
+                //Apliquem l'estil a la capa vectorial
+                //point.setStyle(iconStyle)
+
+
+                //Crea variable mapa, amb dues capes una capa amb el mapa OSM i la capa vectorial
+                //Defineix targer (contenidor de l'HTML on es situa el mapa)
+                //Defineix la vista del mapa, tipus de sistema de cordenades centre del mapa (coordenades que estaran al centre), zoom inicial, zoom minim i zoom maxim
+                this.map = new ol.Map({
+                    layers: [new ol.layer.Tile({ source: new ol.source.OSM() }), this.vectorLayer],
+                    /*interactions:ol.interaction.defaults({
+                        doubleClickZoom: false,
+                        mouseWheelZoom: false,
+                        keyboardZoom: false
+                    }),*/
+                    /*controls: ol.control.defaults({
+                  zoom: false,
+                  attribution: false,
+                  rotate: false
+                      }),*/
+                    target: document.getElementById('content'),
+                    view: new ol.View({
+                        projection: 'EPSG:4326',
+                        center: [1.7310788, 41.2220107],
+                        zoom: this.zoomInicial,
+                        minZoom: this.minzm,
+                        maxZoom: this.maxzm
+
+                    })
+                })
+
             }
         }
     })
