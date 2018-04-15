5eb7adb6d1f55bcb9362d7ef2fc1b2d8dd377209
ricardTorrents <torrents279@gmail.com>
2018-04-15 20:19:46 +0200

    implementacio Detall

--- public/javascripts/visint.js
+++ public/javascripts/visint.js
@@ -15,7 +15,12 @@ window.addEventListener('load', function () {
             minzm: 17,
             vectorLayer: '',
             iconstyle: '',
-            map: ''
+            map: '',
+            deviceInfo:[],
+            deviceAtributes:[],
+            deviceSensors:[],
+            atributesNames:["latitude","longitude","creationDate","name","_id","modificationDate","type","active"],
+            atributesTraductionNames:["Latitud","Longitud","Creacion","Nombre","Identificador","Modificacion","Tipo","Activo",]
         },
         mounted: function () {
             this.loadMap()
@@ -34,7 +39,9 @@ window.addEventListener('load', function () {
 
                 axios.get(this.base_url_api + 'devices').then(function (response) {
                     self.devices = response.data
-                    self.devices.forEach(function (device, index) {
+                    self.devices.filter(function(device){
+                        return device.lastInfo!=null || device.lastInfo!=undefined
+                    }).forEach(function (device, index) {
                         if (index % 2 == 0)
                             self.devices_column1.push(device)
                         else
@@ -129,7 +136,7 @@ window.addEventListener('load', function () {
                     name: "vector",
                     source: new ol.source.Vector({
                         features: [
-                           
+
                         ]
                     })
 
@@ -137,18 +144,18 @@ window.addEventListener('load', function () {
 
 
                 //Define the style of vectorial layer
-               /* this.iconStyle = new ol.style.Style({
-                        image: new ol.style.Icon( /** @type {olx.style.IconOptions}**/ /*({
-			    	anchor: [0.5, 46],
-			    	anchorXUnits: 'fraction',
-			    	anchorYUnits: 'pixels',
-			    	opacity: 0.75,
-			    	src: ''
-	    		}))
-    		});*/
+                /* this.iconStyle = new ol.style.Style({
+                         image: new ol.style.Icon( /** @type {olx.style.IconOptions}**/ /*({
+            anchor: [0.5, 46],
+            anchorXUnits: 'fraction',
+            anchorYUnits: 'pixels',
+            opacity: 0.75,
+            src: ''
+        }))
+    });*/
 
                 //Aply the style to vectorial layer
-             //  this.vectorLayer.setStyle(this.iconStyle)
+                //  this.vectorLayer.setStyle(this.iconStyle)
 
 
                 //Start variable map 
@@ -176,21 +183,82 @@ window.addEventListener('load', function () {
                 axios.get(this.base_url_api + 'devices').then(function (response) {
                     self.devices = response.data
                     self.devices.forEach(function (device) {
-                        
-                        if(device.lastInfo){
-                            let source=self.vectorLayer.getSource();
+
+                        if (device.lastInfo) {
+                            let source = self.vectorLayer.getSource();
                             source.addFeature(new ol.Feature({
-                                name:device.name,
-                                geometry: new ol.geom.Point([device.lastInfo[0].longitude,device.lastInfo[0].latitude])
+                                name: device.name,
+                                geometry: new ol.geom.Point([device.lastInfo[0].longitude, device.lastInfo[0].latitude])
                             }))
                         }
-                        
+
 
                     });
                 }).catch(function (error) {
                     console.log(error)
                 })
+            },
+            deviceDetail: function (idDevice) {
+                document.getElementsByClassName('device').innerHTML=''
+                document.getElementById('deviceColu1').style.display='none'
+                this.devices_column1=[]
+                this.devices_column2=[]
+                this.getDevices()
+
+                document.getElementById('deviceColu1').style.display='inherit'
+                document.getElementById('deviceColu2').style.display='inherit'
+               /* let self=this
+               
+                
+                axios.get(this.base_url_api + 'devices/'+idDevice).then(function (response) {
+                    
+                self.selected_device=response.data
+                let keys=Object.keys(self.selected_device)
+               
+                keys.forEach(function(k){
+                    if((k!="lastInfo")&&(k!="__v")){
+                        self.deviceInfo.push(self.atributesTraductionNames[self.atributesNames.indexOf(k)])
+                        self.deviceInfo.push(self.selected_device[k])
+                        self.deviceAtributes.push(self.deviceInfo)
+                        self.deviceInfo=[]
+                    }
+                })
+                
+                if(self.selected_device.lastInfo){
+                    let keysSensors=Object.keys(self.selected_device.lastInfo)
+               
+                    keysSensors.forEach(function(k){
+                        if(k!="date"){
+                            self.deviceInfo.push(self.atributesTraductionNames[self.atributesNames.indexOf(k)])
+                            
+                            self.deviceInfo.push( self.deviceInfo.push(self.selected_device.lastInfo[k]))
+                            self.deviceSensors.push(self.deviceInfo)
+                            self.deviceInfo=[]
+                        }
+                    })
+                }
+                
+                document.getElementById("devices").style.display = "none"
+                document.getElementById("filter").style.display = "none"
+                document.getElementById("detail").style.display = "inherit"
+                })
+                
+                */
+            },
+            closeDetaill:function(){
+                let self=this
+                self.deviceAtributes=[]
+                self.deviceInfo=[]
+                self.deviceSensors=[]
+                document.getElementsByClassName("blocInfo").innerHTML=""
+                document.getElementById("detail").style.display = "inherit"
+                document.getElementById("devices").style.display = "inherit"
+                document.getElementById("filter").style.display = "inherit"
+                
             }
+
         }
+
+
     })
 })
