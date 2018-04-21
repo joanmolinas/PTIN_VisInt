6fecfff4d8e9af09b98bd52b72bb6f6ed01d68ce
Joan Molinas Ramon <joanmramon@gmail.com>
2018-04-21 10:58:36 +0200

    bcrypt added to sign in & signout

--- routes/auth.js
+++ routes/auth.js
@@ -2,23 +2,29 @@ express = require('express')
 router = express.Router()
 User = require('../models/User')
 service = require("../handlers/token-service")
+encryption = require("../handlers/encryption")
 
-router.post("/signin", function(req, res, next) {
+router.post("/signin", async function(req, res, next) {
     let username = req.body.username
     let password = req.body.password
     if (!username && !password) {
         res.status(400).send({ 'message': 'Username or password missing' })
         return
     }
-
-    let request = { "username": username, "password": password }
-    User.findOne(request, "-password")
-    .then(user => {
+    let request = { "username": username}
+    User.findOne(request)
+    .then(async user => {
         if (user) {
+            let match = await encryption.compare(password, user.password)
+            if(!match) res.status(400).send({ 'message': "User doesn't exists" })
+
             let token = service.createToken(user)
             user.token = token
             user.save()
-            .then(u => res.status(200).send({ 'data': u }))
+            .then(u => {
+                delete u.password
+                res.status(200).send({ 'data': u })
+            })
             .catch(e => { res.status(500).send({'message': 'Internal error'})})
         }
         else res.status(400).send({ 'message': "User doesn't exists" })
@@ -29,20 +35,19 @@ router.post("/signin", function(req, res, next) {
     })
 })
 
-router.post("/signup", function(req, res, next) {
+router.post("/signup", async function(req, res, next) {
     let username = req.body.username
     let password = req.body.password
     if (!username || !password) {
-        console.log("hey");
         res.status(400).send({ 'message': 'Username or password missing' })
         return
     }
 
+    let hashPassword = await encryption.encrypt(password)
     let user = new User({
     	username: req.body.username,
-    	password: req.body.password
+    	password: hashPassword
     })
-
 
     user.save()
     .then(u => {
@@ -56,12 +61,11 @@ router.post("/signup", function(req, res, next) {
         .catch(e => {
             res.status(500).send({'message': 'Internal error'})
         })
-
     })
     .catch(e => {
+        console.log(e);
         res.status(400).send({'message': 'Username already exists'})
     })
-
 })
 
 module.exports = router
