4d962e927961b2fe1d4191581e5d4e140bcc3c4e
Nil22 <b3salvat@gmail.com>
2018-05-15 17:33:51 +0200

    Receive notifications via socket

--- handlers/socket-handler.js
+++ handlers/socket-handler.js
@@ -10,6 +10,10 @@ function connect(http) {
         let dId = socket.handshake.query['id']
         if (dId) { sockets[dId] = socket }
         onDisconnect(socket)
+	fire(socket)
+	heart_attack(socket)
+	high_temp(socket)
+	low_temp(socket)
     })
 }
 
@@ -21,6 +25,33 @@ function onDisconnect(socket) {
     })
 }
 
+// nil
+function fire(socket) {
+    socket.on('fire', () => {
+        console.log('Fire notification received')
+    })
+}
+
+// nil
+function heart_attack(socket) {
+    socket.on('heart_attack', () => {
+	console.log('Heart atack notification received')
+    })
+}
+
+// nil
+function high_temp(socket) {
+    socket.on('high_temp', () => {
+	console.log('High temperature notification received')
+    })
+}
+
+// nil
+function low_temp(socket) {
+    socket.on('low_temp', () => {
+	console.log('Low temperature notification received')
+    })
+} 
 
 function deviceWasUpdated() {
     io.emit('refreshDevicesTable', { for: 'everyone' });
